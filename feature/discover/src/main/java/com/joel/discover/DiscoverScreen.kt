3be569111dd55d5467599cbf225482e811a31612
joelmuraguri <joelkmuraguri21@gmail.com>
2023-06-18 23:07:17 +0300

    implement tabs

--- feature/discover/src/main/java/com/joel/discover/DiscoverScreen.kt
+++ feature/discover/src/main/java/com/joel/discover/DiscoverScreen.kt
@@ -1,6 +1,5 @@
 package com.joel.discover
 
-import android.content.res.Configuration
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -8,28 +7,31 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.material3.Button
 import androidx.compose.material3.Card
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Tab
+import androidx.compose.material3.TabRow
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.scale
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.RectangleShape
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.paging.LoadState
 import androidx.paging.PagingData
 import androidx.paging.compose.collectAsLazyPagingItems
-import androidx.paging.compose.itemContentType
-import androidx.paging.compose.itemKey
+import androidx.paging.compose.items
 import coil.compose.AsyncImagePainter
 import coil.compose.SubcomposeAsyncImage
 import coil.compose.SubcomposeAsyncImageContent
@@ -39,137 +41,264 @@ import kotlinx.coroutines.flow.Flow
 
 
 @Composable
-fun DiscoverScreen(){
-//    Box(
-//        contentAlignment = Alignment.Center,
-//        modifier = Modifier
-//            .fillMaxSize()
-//    ) {
-//        Text(text = "DISCOVER", fontSize = 25.sp)
-//    }
-
-    PopularContent()
+fun DiscoverScreen(
+    discoverViewModel: DiscoverViewModel = hiltViewModel()
+){
+    TabLayout(viewModel = discoverViewModel)
 }
 
 @Composable
-fun PopularContent(
-    viewModel: DiscoverViewModel = hiltViewModel()
+fun PopularFilms(
+    viewModel: DiscoverViewModel
 ){
     Box(contentAlignment = Alignment.Center) {
         Column {
             Text(
-                text = "Popular Tv Shows",
+                text = "Popular Films",
                 style = MaterialTheme.typography.headlineMedium
             )
             Spacer(modifier = Modifier.height(4.dp))
-            LazyRowScrollableItems(pagingItems = viewModel.popularFilms )
+            PopularFilmsScrollableItems(popularFilms = viewModel.popularFilms)
         }
     }
 }
-
 
 @Composable
-fun LazyRowScrollableItems(
-    pagingItems : Flow<PagingData<FilmDTO>>
+fun PopularFilmsScrollableItems(
+    popularFilms : Flow<PagingData<FilmDTO>>
 ){
+    val lazyFilmItems = popularFilms.collectAsLazyPagingItems()
 
-    val popularFilms = pagingItems.collectAsLazyPagingItems()
+    LazyRow(
+        modifier = Modifier.fillMaxWidth()
+    ){
+        items(lazyFilmItems){ filmDTO ->
+            FilmItem(filmDTO = filmDTO!!)
+        }
 
-    Box(
-        contentAlignment = Alignment.Center,
-        modifier = Modifier
-            .fillMaxWidth()
-            .height(195.dp)
-    ) {
-        LazyRow(
-            modifier = Modifier.fillMaxWidth()
-        ){
-            items(count = popularFilms.itemCount, key = popularFilms.itemKey(), contentType = popularFilms.itemContentType()
-    ) { index ->
-        val item = popularFilms[index]
-                FilmItem(filmDTO = item!!)
+        lazyFilmItems.apply {
+            when{
+
+                loadState.refresh is LoadState.Loading -> {
+                    item {
+                        Box(
+                            contentAlignment = Alignment.Center,
+                            modifier = Modifier.fillMaxWidth()
+                        ) {
+                            CircularProgressIndicator(
+                                modifier = Modifier
+                                    .scale(0.5f)
+                                    .align(Alignment.Center)
+                                    .fillMaxWidth(),
+                                color = Color.Yellow
+                            )
+                        }
+
+                    }
+                }
+                loadState.append is LoadState.Loading -> {
+                    item {
+                        Box(
+                            contentAlignment = Alignment.Center,
+                            modifier = Modifier.fillMaxWidth()
+                        ) {
+                            CircularProgressIndicator(
+                                modifier = Modifier
+                                    .scale(0.5f)
+                                    .align(Alignment.Center)
+                                    .fillMaxWidth(),
+                                color = Color.Yellow
+                            )
+                        }
+                    }
+                }
+                loadState.refresh is LoadState.Error -> {
+                    val error = lazyFilmItems.loadState.refresh as LoadState.Error
+                    item {
+                        Box(
+                            modifier = Modifier.fillMaxSize(),
+                            contentAlignment = Alignment.Center
+                        ){
+                            Column(
+                                verticalArrangement = Arrangement.Center,
+                                horizontalAlignment = Alignment.CenterHorizontally
+                            ) {
+                                error.error.localizedMessage?.let {
+                                    Text(
+                                        text = it,
+                                        color = Color.Red
+                                    )
+                                }
+                                Spacer(modifier = Modifier.height(8.dp))
+                                Button(onClick = { refresh() }) {
+                                    Text(text = "Retry")
+                                }
+                            }
+                        }
+                    }
+                }
+                loadState.append is LoadState.Error -> {
+                    val error = lazyFilmItems.loadState.append as LoadState.Error
+                    item {
+                        Column(
+                            verticalArrangement = Arrangement.Center,
+                            horizontalAlignment = Alignment.CenterHorizontally
+                        ) {
+                            error.error.localizedMessage?.let {
+                                Text(
+                                    text = it,
+                                    color = Color.Red
+                                )
+                            }
+                            Spacer(modifier = Modifier.height(8.dp))
+                            Button(onClick = { refresh() }) {
+                                Text(text = "Retry")
+                            }
+                        }
+                    }
+                }
             }
-
-           popularFilms.apply {
-               when{
-                   loadState.refresh is LoadState.Loading ->{
-                       item {
-                           Box(
-                               contentAlignment = Alignment.Center,
-                               modifier = Modifier.fillMaxWidth()
-                           ) {
-                               CircularProgressIndicator(
-                                   modifier = Modifier
-                                       .scale(0.5f)
-                                       .align(Alignment.Center)
-                                       .fillMaxWidth(),
-                                   color = Color.Yellow
-                               )
-                           }
-
-                       }
-                   }
-                   loadState.append is LoadState.Loading -> {
-                       item {
-                           Box(
-                               contentAlignment = Alignment.Center,
-                               modifier = Modifier.fillMaxWidth()
-                           ) {
-                               CircularProgressIndicator(
-                                   modifier = Modifier
-                                       .scale(0.5f)
-                                       .align(Alignment.Center)
-                                       .fillMaxWidth(),
-                                   color = Color.Yellow
-                               )
-                           }
-                       }
-                   }
-                   loadState.refresh is LoadState.Error -> {
-                       val error = popularFilms.loadState.refresh as LoadState.Error
-                       item {
-                           Column(
-                               verticalArrangement = Arrangement.Center,
-                               horizontalAlignment = Alignment.CenterHorizontally
-                           ) {
-                               error.error.localizedMessage?.let {
-                                   Text(
-                                       text = it,
-                                       color = Color.Red
-                                   )
-                               }
-                               Spacer(modifier = Modifier.height(8.dp))
-                               Button(onClick = { refresh() }) {
-                                   Text(text = "Retry")
-                               }
-                           }
-                       }
-                   }
-                   loadState.append is LoadState.Error -> {
-                       val error = popularFilms.loadState.append as LoadState.Error
-                       item {
-                           Column(
-                               verticalArrangement = Arrangement.Center,
-                               horizontalAlignment = Alignment.CenterHorizontally
-                           ) {
-                               error.error.localizedMessage?.let {
-                                   Text(
-                                       text = it,
-                                       color = Color.Red
-                                   )
-                               }
-                               Spacer(modifier = Modifier.height(8.dp))
-                               Button(onClick = { refresh() }) {
-                                   Text(text = "Retry")
-                               }
-                           }
-                       }
-                   }
-               }
-           }
         }
     }
 }
+@Composable
+fun UpcomingFilms(
+    viewModel: DiscoverViewModel
+){
+    Box(contentAlignment = Alignment.Center) {
+        Column {
+            Text(
+                text = "Upcoming Films",
+                style = MaterialTheme.typography.headlineMedium
+            )
+            Spacer(modifier = Modifier.height(4.dp))
+            UpcomingFilmsScrollableItems(popularFilms = viewModel.upcomingFilms)
+        }
+    }
+}
+
+@Composable
+fun UpcomingFilmsScrollableItems(
+    popularFilms : Flow<PagingData<FilmDTO>>
+){
+    val lazyFilmItems = popularFilms.collectAsLazyPagingItems()
+
+    LazyRow(
+        modifier = Modifier.fillMaxWidth()
+    ){
+        items(lazyFilmItems){ filmDTO ->
+            FilmItem(filmDTO = filmDTO!!)
+        }
+
+        lazyFilmItems.apply {
+            when{
+
+                loadState.refresh is LoadState.Loading -> {
+                    item {
+                        Box(
+                            contentAlignment = Alignment.Center,
+                            modifier = Modifier.fillMaxWidth()
+                        ) {
+                            CircularProgressIndicator(
+                                modifier = Modifier
+                                    .scale(0.5f)
+                                    .align(Alignment.Center)
+                                    .fillMaxWidth(),
+                                color = Color.Yellow
+                            )
+                        }
+
+                    }
+                }
+                loadState.append is LoadState.Loading -> {
+                    item {
+                        Box(
+                            contentAlignment = Alignment.Center,
+                            modifier = Modifier.fillMaxWidth()
+                        ) {
+                            CircularProgressIndicator(
+                                modifier = Modifier
+                                    .scale(0.5f)
+                                    .align(Alignment.Center)
+                                    .fillMaxWidth(),
+                                color = Color.Yellow
+                            )
+                        }
+                    }
+                }
+                loadState.refresh is LoadState.Error -> {
+                    val error = lazyFilmItems.loadState.refresh as LoadState.Error
+                    item {
+                        Box(
+                            modifier = Modifier.fillMaxSize(),
+                            contentAlignment = Alignment.Center
+                        ){
+                            Column(
+                                verticalArrangement = Arrangement.Center,
+                                horizontalAlignment = Alignment.CenterHorizontally
+                            ) {
+                                error.error.localizedMessage?.let {
+                                    Text(
+                                        text = it,
+                                        color = Color.Red
+                                    )
+                                }
+                                Spacer(modifier = Modifier.height(8.dp))
+                                Button(onClick = { refresh() }) {
+                                    Text(text = "Retry")
+                                }
+                            }
+                        }
+                    }
+                }
+                loadState.append is LoadState.Error -> {
+                    val error = lazyFilmItems.loadState.append as LoadState.Error
+                    item {
+                        Column(
+                            verticalArrangement = Arrangement.Center,
+                            horizontalAlignment = Alignment.CenterHorizontally
+                        ) {
+                            error.error.localizedMessage?.let {
+                                Text(
+                                    text = it,
+                                    color = Color.Red
+                                )
+                            }
+                            Spacer(modifier = Modifier.height(8.dp))
+                            Button(onClick = { refresh() }) {
+                                Text(text = "Retry")
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun TabLayout(viewModel: DiscoverViewModel) {
+    val tabIndex by viewModel.tabIndex.collectAsState()
+    Column(modifier = Modifier.fillMaxWidth()) {
+        TabRow(selectedTabIndex = tabIndex, modifier = Modifier
+            .padding(end = 100.dp)
+            .height(90.dp)
+        ) {
+            viewModel.tabs.forEachIndexed { index, title ->
+                Tab(text = { Text(title) },
+                    selected = tabIndex == index,
+                    onClick = { viewModel.updateTabIndex(index) },
+                )
+            }
+        }
+        when (tabIndex) {
+            0 -> PopularFilms(viewModel = viewModel)
+            1 -> UpcomingFilms(viewModel = viewModel)
+        }
+    }
+}
+
+
 
 
 @Composable
@@ -178,7 +307,9 @@ fun FilmItem(
 ){
     Column(
         verticalArrangement = Arrangement.Center,
-        horizontalAlignment = Alignment.CenterHorizontally
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = Modifier
+            .padding(12.dp)
     ) {
         
         Card(
@@ -208,15 +339,12 @@ fun FilmItem(
                 }
             }
         }
+        
+        Text(
+            text = filmDTO.title,
+            overflow = TextOverflow.Clip
+        )
+
     }
 }
 
-@Preview("Film Item contents")
-@Preview("Film Item (dark)", uiMode = Configuration.UI_MODE_NIGHT_YES)
-@Composable
-fun PreviewAFilmItemRail() {
-
-
-
-
-}
--- feature/discover/src/main/java/com/joel/discover/DiscoverViewModel.kt
+++ feature/discover/src/main/java/com/joel/discover/DiscoverViewModel.kt
@@ -1,5 +1,6 @@
 package com.joel.discover
 
+import androidx.compose.foundation.gestures.DraggableState
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import androidx.paging.Pager
@@ -12,6 +13,9 @@ import com.example.network.paging.UpcomingFimSource
 import com.example.network.repo.PagedFilmsRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
 import javax.inject.Inject
 
 @HiltViewModel
@@ -27,6 +31,32 @@ class DiscoverViewModel @Inject constructor(
     val upcomingFilms : Flow<PagingData<FilmDTO>> = Pager(PagingConfig(pageSize = 20)){
         UpcomingFimSource(repository)
     }.flow.cachedIn(viewModelScope)
+
+    private val _tabIndex: MutableStateFlow<Int> = MutableStateFlow(0)
+    val tabIndex: StateFlow<Int> get() = _tabIndex.asStateFlow()
+
+    val tabs = listOf("Now Playing", "Coming Soon" )
+
+    private var isSwipeToTheLeft: Boolean = false
+    private val draggableState = DraggableState { delta ->
+        isSwipeToTheLeft = delta > 0
+    }
+
+    private val _dragState = MutableStateFlow(draggableState)
+    val dragState: StateFlow<DraggableState> get() = _dragState.asStateFlow()
+
+    fun updateTabIndexBasedOnSwipe() {
+        val currentIndex = _tabIndex.value
+        val newIndex = when (isSwipeToTheLeft) {
+            true -> Math.floorMod(currentIndex + 1, tabs.size)
+            false -> Math.floorMod(currentIndex - 1, tabs.size)
+        }
+        _tabIndex.value = newIndex
+    }
+
+    fun updateTabIndex(i: Int) {
+        _tabIndex.value = i
+    }
 
 
 
