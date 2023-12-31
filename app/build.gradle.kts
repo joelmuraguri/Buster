e258257272b3b5755ea7707e120b60c931d4de67
joelmuraguri <joelkmuraguri21@gmail.com>
2023-12-31 20:46:35 +0300

    add needed dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,87 +1,21 @@
-import org.jetbrains.kotlin.de.undercouch.gradle.tasks.download.org.apache.http.conn.util.PublicSuffixMatcherLoader.load
-
+@Suppress("DSL_SCOPE_VIOLATION")
 plugins {
-    id("com.android.application")
-    id("org.jetbrains.kotlin.android")
-}
+    alias(libs.plugins.buster.android.application)
+    alias(libs.plugins.buster.kotlin.android)
+    alias(libs.plugins.buster.android.compose)
+    alias(libs.plugins.buster.android.sharedLibrary)
+    alias(libs.plugins.buster.android.hilt)
 
-//val credentials = rootProject.file("local.properties")
-//val credentialProperty = java.util.Properties().apply {
-//    load(credentials.inputStream())
-//}
+}
 
 android {
     namespace = "com.joel.buster"
-    compileSdk = 33
 
-    defaultConfig {
-        applicationId = "com.joel.buster"
-        minSdk = 24
-        targetSdk = 33
-        versionCode = 1
-        versionName = "1.0"
-
-        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
-        vectorDrawables {
-            useSupportLibrary = true
-        }
 
 //        buildConfigField("String", "BASE_URL", "\"https://api.themoviedb.org/\"")
 //        buildConfigField("String", "SMALL_IMAGE_URL", "\"https://image.tmdb.org/t/p/w200\"")
 //        buildConfigField("String", "LARGE_IMAGE_URL", "\"https://image.tmdb.org/t/p/w500\"")
 //        buildConfigField("String", "ORIGINAL_IMAGE_URL", "\"https://image.tmdb.org/t/p/original\"")
 
-
-//        val properties = java.util.Properties().apply {
-//            load(project.rootProject.file("local.properties").inputStream())
-//        }
-//        buildConfigField("String", "TMDB_API_KEY", "\"${properties.getProperty("TMDB_API_KEY")}\"")
-
-    }
-
-    buildTypes {
-        getByName("release") {
-            isMinifyEnabled = true
-            proguardFiles (getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
-        }
-    }
-
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_17
-        targetCompatibility = JavaVersion.VERSION_17
-    }
-    kotlinOptions {
-        jvmTarget = "17"
-    }
-    buildFeatures {
-        compose = true
-    }
-    composeOptions {
-        kotlinCompilerExtensionVersion = "1.4.7"
-    }
-    packagingOptions {
-        resources {
-            excludes += "/META-INF/{AL2.0,LGPL2.1}"
-        }
-    }
 }
 
-dependencies {
-
-
-    implementation(libs.androidx.tracing.ktx)
-    implementation(libs.androidx.compose.runtime.tracing)
-    implementation(libs.androidx.compose.material3.windowSizeClass)
-    implementation(libs.androidx.navigation)
-    implementation (libs.androidx.core.ktx)
-    implementation (libs.bundles.lifecycle)
-    implementation (libs.androidx.activity.compose)
-    implementation (platform(libs.androidx.compose.bom))
-    implementation (libs.bundles.compose)
-    testImplementation (libs.junit)
-    androidTestImplementation (libs.bundles.junit.test)
-    androidTestImplementation (libs.androidx.test.espresso)
-    androidTestImplementation (platform(libs.androidx.compose.bom))
-    debugImplementation (libs.bundles.compose.debug)
-
-}
