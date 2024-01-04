7b759fd9d8dcfa91adcc5cba562c5ee699b57b70
joelmuraguri <joelkmuraguri21@gmail.com>
2024-01-04 11:30:11 +0300

    set up timber

--- app/src/main/java/com/joel/buster/BusterApp.kt
+++ app/src/main/java/com/joel/buster/BusterApp.kt
@@ -2,6 +2,16 @@ package com.joel.buster
 
 import android.app.Application
 import dagger.hilt.android.HiltAndroidApp
+import timber.log.Timber
 
 @HiltAndroidApp
-class BusterApp :Application()
+class BusterApp :Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+
+        if (BuildConfig.DEBUG){
+            Timber.plant(Timber.DebugTree())
+        }
+    }
+}
