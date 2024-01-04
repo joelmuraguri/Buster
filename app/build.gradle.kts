df67e2794d09d744cecc411a6256ea835e95d06f
joelmuraguri <joelkmuraguri21@gmail.com>
2024-01-04 11:28:49 +0300

    set up build config

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,3 +1,5 @@
+import java.util.Properties
+
 @Suppress("DSL_SCOPE_VIOLATION")
 plugins {
     alias(libs.plugins.buster.android.application)
@@ -5,7 +7,17 @@ plugins {
     alias(libs.plugins.buster.android.compose)
     alias(libs.plugins.buster.android.sharedLibrary)
     alias(libs.plugins.buster.android.hilt)
+    alias(libs.plugins.buster.android.ktor.client)
 
+}
+
+val localPropertiesFile = rootProject.file("local.properties")
+val localProperties = Properties().apply {
+    if (localPropertiesFile.exists()) {
+        localPropertiesFile.inputStream().use { stream ->
+            load(stream)
+        }
+    }
 }
 
 android {
@@ -17,5 +29,19 @@ android {
 //        buildConfigField("String", "LARGE_IMAGE_URL", "\"https://image.tmdb.org/t/p/w500\"")
 //        buildConfigField("String", "ORIGINAL_IMAGE_URL", "\"https://image.tmdb.org/t/p/original\"")
 
+    buildTypes {
+        getByName("debug") {
+            buildConfigField("String", "TMDB_API_KEY", "\"${localProperties.getProperty("TMDB_API_KEY")}\"")
+        }
+        debug {
+            // configuration for debug builds
+            buildConfigField("boolean", "DEBUG", "true")
+        }
+    }
+
+}
+
+dependencies {
+    implementation(project(":core:network"))
 }
 
